fdd9f9e64750735546d4603f19b37b23c4bd1944
ishabhardwaj <ishabhardwaj99@gmail.com>
2020-11-12 13:19:03 +0530

    First commit

---
+++ Webdemo/Controllers/WeatherForecastController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System.Net;
+using System.Web;
+using System.Net.Http;
+
+
+namespace Webdemo.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]/[Action]")]
+    public class WeatherForecastController : ControllerBase
+    {
+     static List<string> names = new List<string>()
+         {
+            "harshit","naina","isha"
+         };
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return names;
+        }
+        [HttpGet]
+        public string Get (int id) {
+            return names[id];
+        }
+        [HttpPost]
+        public void Post([FromBody]string value)
+        {
+            names.Add(value);
+        }
+        [HttpPut]
+        public void Put(int id,[FromBody]string value)
+        {
+            names[id] = value;
+        }
+       [HttpDelete]
+        public void Delete(int id) 
+        {
+            names.RemoveAt(id);
+        }
+        
+    }
+}
